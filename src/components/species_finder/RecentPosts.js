5ec882e75a61e1af7ba8d40c7f2032b9162aa6a6
Parker Lillywhite <parkerlillywhite@Parkers-MBP.fastel.com>
2023-03-10 12:53:38 -0700

    redux thunk

--- src/components/species_finder/RecentPosts.js
+++ src/components/species_finder/RecentPosts.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-const RecentPosts = () => {
-    return(
-        <div className="resultsContainer">
-            <div className="recentPostsHeading"></div>
-            <ul className="resultsList">
-                <li>nonsense</li>
-                <li>nonsense</li>
-                <li>nonsense</li>
-            </ul>
-        </div>
-    )
+import * as actions from '../.././actions'
+
+class RecentPosts extends Component {
+
+    componentWillMount(){
+        this.props.fetchRecentPosts();
+    }
+    render(){
+        return(
+            <div className="resultsContainer">
+                <div className="recentPostsHeading"></div>
+                <ul className="resultsList">
+                    <li>nonsense</li>
+                    <li>nonsense</li>
+                    <li>nonsense</li>
+                </ul>
+            </div>
+        )
+    }
 }
-export default RecentPosts;
+export default connect(null, actions)(RecentPosts);
