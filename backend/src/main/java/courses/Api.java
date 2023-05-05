0b694d08c0603c69c1e42ac0219a0f9866b4d939
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-05-04 22:02:21 -0600

    failing api test

--- backend/src/main/java/courses/Api.java
+++ backend/src/main/java/courses/Api.java
@@ -5,13 +5,23 @@ import org.sql2o.Sql2o;
 import courses.dao.Sql2oCourseDao;
 import courses.dao.CourseDao;
 
-import static spark.Spark.post;
-import static spark.Spark.get;
-import static spark.Spark.after;
+import static spark.Spark.*;
 
 public class Api {
     public static void main(String[] args){
-        Sql2o sql2o = new Sql2o("jdbc:h2:~/reviews.db;INIT=RUNSCRIPT from 'classpath:db/init.sql'", "", "");
+        String dataSource = "jdbc:h2:~/reviews.db";
+
+        if(args.length > 0){
+            if(args.length != 2){
+                System.out.println("Java Api <port> <datasource>");
+                System.exit(0);
+            }
+            port(Integer.parseInt(args[0]));
+            dataSource = args[1];
+        }
+        Sql2o sql2o = new Sql2o(
+                String.format("%s;INIT=RUNSCRIPT from 'classpath:db/init.sql'", dataSource),
+                "", "");
         CourseDao courseDao = new Sql2oCourseDao(sql2o);
         Gson gson = new Gson();
 
--- backend/src/test/java/course/Sql2oCourseDaoTest.java
+++ backend/src/test/java/course/Sql2oCourseDaoTest.java
@@ -1,4 +1,4 @@
-package courseDao;
+package course;
 
 
 
@@ -10,13 +10,14 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import courses.Course;
 import courses.dao.Sql2oCourseDao;
+import org.junit.jupiter.api.BeforeEach;
 
 public class Sql2oCourseDaoTest {
 
     private Sql2oCourseDao dao;
     private Connection connection;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     public void setUp() throws Exception {
         //on start up this will run the script to create our tables which is all good fun
         String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/init.sql'";
