505de0e200aecc102edaf97032c6eba86220e383
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-05-16 17:33:12 -0600

    post and get for reviews

--- backend/src/main/java/courses/Api.java
+++ backend/src/main/java/courses/Api.java
@@ -1,7 +1,10 @@
 package courses;
 
 import com.google.gson.Gson;
+import courses.dao.ReviewDao;
+import courses.dao.Sql2oReviewDao;
 import courses.exc.ApiException;
+import courses.exc.DaoException;
 import org.sql2o.Sql2o;
 import courses.dao.Sql2oCourseDao;
 import courses.dao.CourseDao;
@@ -27,6 +30,7 @@ public class Api {
                 String.format("%s;INIT=RUNSCRIPT from 'classpath:db/init.sql'", dataSource),
                 "", "");
         CourseDao courseDao = new Sql2oCourseDao(sql2o);
+        ReviewDao reviewDao = new Sql2oReviewDao(sql2o);
         Gson gson = new Gson();
 
         post("/courses", "application/json", (req, res) -> {
@@ -46,6 +50,27 @@ public class Api {
                 throw new ApiException(404, "Could not find course id " + id);
             }
             return course;
+        }, gson::toJson);
+
+        post("/courses/:courseId/reviews", "application/json", (req, res) -> {
+            int courseId = Integer.parseInt(req.params("courseId"));
+            Review review = gson.fromJson(req.body(), Review.class);
+            review.setId(courseId);
+            try {
+                reviewDao.add(review);
+            } catch (DaoException exception) {
+                throw new ApiException(500, exception.getMessage());
+            }
+            res.status(201);
+            return review;
+        }, gson::toJson);
+
+        get("/courses/:courseId/reviews", "application/Json",
+                (req, res) -> reviewDao.findAll() ,gson::toJson);
+
+        get("/course/:courseId/reviews", "application/Json", (req, res) -> {
+            int courseId = Integer.parseInt(req.params("courseId"));
+            return reviewDao.findByCourseId(courseId);
         }, gson::toJson);
 
         exception(ApiException.class, (exc, req, res) -> {
