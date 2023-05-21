de0d9079cb8cb8058dce4944f053e482468853f2
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-05-21 13:16:49 -0600

    failing api test

--- backend/src/test/java/user/UserApiTest.java
+++ backend/src/test/java/user/UserApiTest.java
@@ -4,6 +4,7 @@ import User.Login.dao.Sql2oUserDao;
 import User.Login.dao.UserDao;
 import User.Login.User;
 import courses.exc.DaoException;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,9 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.sql2o.Sql2oException;
 
-import static io.restassured.RestAssured.when;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class UserApiTest {
 
@@ -27,15 +34,27 @@ public class UserApiTest {
     @BeforeEach
     public void setUp(){
         RestAssuredMockMvc.mockMvc(mockMvc);
+        userDao = mock(Sql2oUserDao.class);
     }
 
     @Test
-    public void testCreateUserEndpoint() {
-        User user = new User("Borbley", "Bonson");
-        try {
-            when(userDao.add(any(User.class))).thenReturn(user);
-        } catch (DaoException e){
-            throw new MockitoException("problem");
-        }
+    public void testCreateUserEndpoint() throws DaoException {
+
+        String requestPayload = "{ \"userName\": \"DarkSouls\", \"passWord\": \"something\" }";
+
+        doNothing().when(userDao).add(any(User.class));
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestPayload)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .body("userName", equalTo("DarkSouls"))
+                .body("passWord", equalTo("something"));
+
+        verify(userDao, times(1)).add(any(User.class));
+
     }
 }
